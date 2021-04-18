1be166e340bb001246074bcef288b39980ba3029
Elli <elligradney@gmail.com>
2021-04-18 11:38:37 -0500

    adding save pref to local storage

--- script.js
+++ script.js
@@ -8,45 +8,56 @@ const image3 = document.getElementById('image3');
 const toggleText = document.getElementById('toggle-text');
 const textBox = document.getElementById('text-box');
 
-//Dar Mode Styles
-
+//Dark or Light Images 
+const imageMode = (color) => {
+    image1.setAttribute('src', `/img/undraw_proud_coder_${color}.svg`);
+    image2.setAttribute('src', `/img/undraw_feeling_proud_${color}.svg`);
+    image3.setAttribute('src', `/img/undraw_conceptual_idea_${color}.svg`);  
+}
+//Dark Mode Styles
 const darkMode = () => {
-    document.documentElement.setAttribute('data-theme', 'dark');
     nav.style.backgroundColor = 'rgb(0 0 0 /50%';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     // toggleText.textContent = 'Dark Mode';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.setAttribute('src', '/img/undraw_proud_coder_dark.svg');
-    image2.setAttribute('src', '/img/undraw_feeling_proud_dark.svg');
-    image3.setAttribute('src', '/img/undraw_conceptual_idea_dark.svg');
-
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    imageMode('dark');
 }
 
 const lightMode = () => {
-    document.documentElement.setAttribute('data-theme', 'light');;
+    console.log(currentTheme);
     nav.style.backgroundColor = 'rgb(255 255 255 /50%';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     // toggleText.textContent = 'Light Mode';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.setAttribute('src', '/img/undraw_proud_coder_light.svg');
-    image2.setAttribute('src', '/img/undraw_feeling_proud_light.svg');
-    image3.setAttribute('src', '/img/undraw_conceptual_idea_light.svg');
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    imageMode('light');
 }
 
 //Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
-        
-        lightMode();   
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode(); 
+        console.log(currentTheme);  
     }
- 
-}
+ }
 
 //Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+//Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
