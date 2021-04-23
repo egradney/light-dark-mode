a9a9dff6b59d498c72ae489ba34b152329cc7213
Elli <elligradney@gmail.com>
2021-04-23 17:44:47 -0500

    repair img src path

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ let choice = '';
 
 //Dark or Light Images 
 function imageMode(color) {
-    image1.setAttribute('src', `/img/undraw_proud_coder_${color}.svg`);
-    image2.setAttribute('src', `/img/undraw_feeling_proud_${color}.svg`);
-    image3.setAttribute('src', `/img/undraw_conceptual_idea_${color}.svg`);  
+    image1.setAttribute('src', `img/undraw_proud_coder_${color}.svg`);
+    image2.setAttribute('src', `img/undraw_feeling_proud_${color}.svg`);
+    image3.setAttribute('src', `img/undraw_conceptual_idea_${color}.svg`);  
 }
 
 //Light or Dark Elements
